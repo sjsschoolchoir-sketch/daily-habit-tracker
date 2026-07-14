b25dd134c4148b37c142e9975f0c77cf47d3c394
sjsschoolchoir-sketch <sjsschoolchoir@gmail.com>
2026-07-14 14:06:25 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAZoA0pAm7YmHzcOLfmQvut6ZdZpRd6ZpQ",
-  authDomain: "daily-habit-tracker-dd8a8.firebaseapp.com",
-  projectId: "daily-habit-tracker-dd8a8",
-  storageBucket: "daily-habit-tracker-dd8a8.firebasestorage.app",
-  messagingSenderId: "925989942376",
-  appId: "1:925989942376:web:1d709d057645dd5457b7dd"
-};
